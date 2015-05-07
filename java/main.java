c967b4986c115c3006f595aaed5eb78b7242437e
Esteban Leon <eleon182@gmail.com>
2015-05-07 07:08:01 -0700

    java

---
+++ java/main.java
@@ -0,0 +1,8 @@
+public class main {
+
+    public static void main (String[] args) {
+        System.out.println("hi");
+        steve mysteve = new steve();
+        System.out.println(mysteve.getmyint());
+    }
+}
---
+++ java/steve.java
@@ -0,0 +1,10 @@
+public class steve {
+    private int myint;
+
+    public steve() {
+        myint = 1;
+    }
+    public int getmyint() {
+        return myint;
+    }
+}
