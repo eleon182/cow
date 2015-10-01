5b39d7b0bdaf6fbc3b70ee3fd56c475a0c1fb4ff
Esteban Leon <eleon182@gmail.com>
2015-09-30 18:11:06 -0700

    add new route for update sector

--- api/userProfile-route.js
+++ api/userProfile-route.js
@@ -5,15 +5,35 @@ var router = express.Router();
 var userProfile = require('./userProfile');
 
 router.get('/getUser', function(req, res, next) {
-    userProfile.getUser(req.headers).then(function(data){
-        res.send(data);
-    },
-    function(err){
-        res.status(500).send({
-            code: 'notFound',
-            error: 'User not found'
+    userProfile.getUser(req.headers).then(function(data) {
+            res.send(data);
+        },
+        function(err) {
+            res.status(500).send({
+                code: 'notFound',
+                error: 'User not found'
+            });
         });
-    });
 });
 
+router.post('/updateSector', function(req, res, next) {
+    if (!req.body.sector) {
+        res.status(500).send({
+            code: 'missingParameters',
+            error: 'Missing Parameters: sector'
+        });
+    } else {
+        req.body.username = req.headers.username;
+        userProfile.updateSector(req.body).then(function(data) {
+                res.send(data);
+            },
+            function(err) {
+                res.status(500).send({
+                    code: 'notFound',
+                    error: 'User not found',
+                    message: err
+                });
+            });
+    }
+});
 module.exports = router;
