c7483f859962aee78eeab35203e2a710a21125cf
Esteban Leon <eleon182@gmail.com>
2015-10-02 09:13:01 -0700

    update display user

--- api/commandParser/displayUser.js
+++ api/commandParser/displayUser.js
@@ -20,6 +20,12 @@ function buildSlackResponse(input){
     var response = 'User Information';
     response += '\n```';
     response += '\nUser: ' + input.username;
+    response += '\nFighters: ' + input.fighters;
+    response += '\nShields: ' + input.shields;
+    response += '\nCurrency: ' + input.currency;
+    response += '\nFuel: ' + input.fuel;
+    response += '\nAvailable holds: ' + input.holds;
+    response += '\nCreate date: ' + new Date(input.createDate).toDateString();
     response += '\nSector: ' + input.sector;
     response += '\nAdjacent sectors: ' + input.adjacent;
     response += '\n```';
