e0c56e73cc41372d1b5359268b6076e25f2e2175
Esteban Leon <eleon182@gmail.com>
2015-10-02 11:06:33 -0700

    hops

--- api/commandParser/getPath.js
+++ api/commandParser/getPath.js
@@ -17,6 +17,7 @@ function buildSlackResponse(input) {
     var response = 'Compute navigation path';
     response += '\n```';
     response += '\n' + input;
+    response += '\nHops: ' + (input.length - 1);
     response += '\nTranswarp fuel cost: ' + (input.length - 1) * 3;
     response += '\n```';
     return response;
