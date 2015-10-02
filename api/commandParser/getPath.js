299870089bfb34dcf1e41efdef8a750d25285b0b
Esteban Leon <eleon182@gmail.com>
2015-10-02 10:32:30 -0700

    fuel cost

--- api/commandParser/getPath.js
+++ api/commandParser/getPath.js
@@ -17,6 +17,7 @@ function buildSlackResponse(input) {
     var response = 'Compute navigation path';
     response += '\n```';
     response += '\n' + input;
+    response += '\nTranswarp fuel cost: ' + (input.length - 1) * 3;
     response += '\n```';
     return response;
 }
